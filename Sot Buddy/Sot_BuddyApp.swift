f12b7c03e0545353a1e8e7f7762c7cb1c673a86c
Burneikis <87457198+AlexBurneikis@users.noreply.github.com>
2022-06-10 17:48:45 +1000

    Initial Commit

---
+++ Sot Buddy/Sot_BuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sot_BuddyApp.swift
+//  Sot Buddy
+//
+//  Created by Alexander Burneikis on 10/6/2022.
+//
+
+import SwiftUI
+
+@main
+struct Sot_BuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

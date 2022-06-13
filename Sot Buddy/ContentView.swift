af7d91a7a81d04a58a6e8bcc7b5f67e016cd46bb
Burneikis <87457198+AlexBurneikis@users.noreply.github.com>
2022-06-13 10:26:31 +1000

    Working timer

--- Sot Buddy/ContentView.swift
+++ Sot Buddy/ContentView.swift
@@ -65,126 +65,126 @@ struct ContentView: View {
             .navigationBarTitleDisplayMode(.inline)
         }
     }
+}
+
+struct NewVoyage: View {
+    @State private var voyageName = ""
+    @State private var initialGold = ""
+    @State private var initialDoubloons = ""
+    @State private var voyageSheetShow = false
+    @State private var showAlert = false
+    @State private var finalGold = ""
+    @State private var finalDoubloons = ""
     
-    struct NewVoyage: View {
-        @State private var voyageName = ""
-        @State private var initialGold = ""
-        @State private var initialDoubloons = ""
-        @State private var voyageSheetShow = false
-        @State private var showAlert = false
-        @State private var finalGold = ""
-        @State private var finalDoubloons = ""
-        
-        @ObservedObject var managerClass = ManagerClass()
-        
-        func timeString(time: TimeInterval) -> String {
-            let hours = Int(time) / 3600
-            let minutes = Int(time) / 60 % 60
-            let seconds = Int(time) % 60
-            return String(format: "%02i:%02i:%02i", hours, minutes, seconds)
+    @ObservedObject var managerClass = ManagerClass()
+    
+    func timeString(time: TimeInterval) -> String {
+        let hours = Int(time) / 3600
+        let minutes = Int(time) / 60 % 60
+        let seconds = Int(time) % 60
+        return String(format: "%02i:%02i:%02i", hours, minutes, seconds)
+    }
+    
+    func goldProfit() -> Int {
+        if finalGold == "" {
+            return 0
+        } else {
+            if initialGold == "" {
+                return Int(finalGold)!
+            }
+            return Int(finalGold)! - Int(initialGold)!
         }
-        
-        func goldProfit() -> Int {
-            if finalGold == "" {
-                return 0
-            } else {
-                if initialGold == "" {
-                    return Int(finalGold)!
+    }
+    
+    func goldPerHour() -> Int {
+        let hours = Float(managerClass.secondElapsed + 0.1) / 3600
+        return hours == 0 ? 0 : Int(Float(goldProfit())/hours)
+    }
+    
+    func doubloonsPerHour() -> Int {
+        let hours = Float(managerClass.secondElapsed + 0.1) / 3600
+        return hours == 0 ? 0 : Int(Float(doubloonProfit())/hours)
+    }
+    
+    func doubloonProfit() -> Int {
+        if finalDoubloons == "" {
+            return 0
+        } else {
+            if initialDoubloons == "" {
+                return Int(finalDoubloons)!
+            }
+            return Int(finalDoubloons)! - Int(initialDoubloons)!
+        }
+    }
+    
+    var body: some View {
+        Form {
+            Section {
+                TextField("My Voyage", text: $voyageName)
+                TextField("Initial Gold", text: $initialGold)
+                    .keyboardType(.decimalPad)
+                TextField("Initial Doubloons", text: $initialDoubloons)
+                    .keyboardType(.decimalPad)
+            }
+            Section {
+                Button {
+                    voyageSheetShow.toggle()
+                    managerClass.start()
+                } label: {
+                    HStack {
+                        Spacer()
+                        Text("Start")
+                            .foregroundColor(.blue)
+                        Spacer()
+                    }
                 }
-                return Int(finalGold)! - Int(initialGold)!
             }
         }
-        
-        func goldPerHour() -> Int {
-            let hours = Float(managerClass.secondElapsed + 0.1) / 3600
-            return hours == 0 ? 0 : Int(Float(goldProfit())/hours)
-        }
-        
-        func doubloonsPerHour() -> Int {
-            let hours = Float(managerClass.secondElapsed + 0.1) / 3600
-            return hours == 0 ? 0 : Int(Float(doubloonProfit())/hours)
-        }
-        
-        func doubloonProfit() -> Int {
-            if finalDoubloons == "" {
-                return 0
-            } else {
-                if initialDoubloons == "" {
-                    return Int(finalDoubloons)!
+        .navigationBarTitle(Text("New Voyage"))
+        .sheet(isPresented: $voyageSheetShow) {
+            NavigationView {
+                VStack {
+                    Text(timeString(time: TimeInterval(managerClass.secondElapsed)))
+                    Form {
+                        TextField("Final Gold", text: $finalGold)
+                            .keyboardType(.decimalPad)
+                        TextField("Final Doubloons", text: $finalDoubloons)
+                            .keyboardType(.decimalPad)
+                    }
+                    HStack {
+                        Text("+" + String(goldProfit()))
+                        Text(String(goldPerHour()) + "/hour")
+                        Text("+" + String(doubloonProfit()))
+                        Text(String(doubloonsPerHour()) + "/hour")
+                    }
                 }
-                return Int(finalDoubloons)! - Int(initialDoubloons)!
-            }
-        }
-        
-        var body: some View {
-            Form {
-                Section {
-                    TextField("My Voyage", text: $voyageName)
-                    TextField("Initial Gold", text: $initialGold)
-                        .keyboardType(.decimalPad)
-                    TextField("Initial Doubloons", text: $initialDoubloons)
-                        .keyboardType(.decimalPad)
-                }
-                Section {
+                .navigationTitle(Text(voyageName != "" ? voyageName : "My Voyage"))
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
                     Button {
-                        voyageSheetShow.toggle()
-                        managerClass.start()
+                        showAlert.toggle()
+                        managerClass.stop()
                     } label: {
-                        HStack {
-                            Spacer()
-                            Text("Start")
-                                .foregroundColor(.blue)
-                            Spacer()
-                        }
+                        Text("Done")
+                            .bold()
                     }
                 }
             }
-            .navigationBarTitle(Text("New Voyage"))
-            .sheet(isPresented: $voyageSheetShow) {
-                NavigationView {
-                    VStack {
-                        Text(timeString(time: TimeInterval(managerClass.secondElapsed)))
-                        Form {
-                            TextField("Final Gold", text: $finalGold)
-                                .keyboardType(.decimalPad)
-                            TextField("Final Doubloons", text: $finalDoubloons)
-                                .keyboardType(.decimalPad)
-                        }
-                        HStack {
-                            Text("+" + String(goldProfit()))
-                            Text(String(goldPerHour()) + "/hour")
-                            Text("+" + String(doubloonProfit()))
-                            Text(String(doubloonsPerHour()) + "/hour")
-                        }
-                    }
-                    .navigationTitle(Text(voyageName != "" ? voyageName : "My Voyage"))
-                    .navigationBarTitleDisplayMode(.inline)
-                    .toolbar {
-                        Button {
-                            showAlert.toggle()
-                            managerClass.stop()
-                        } label: {
-                            Text("Done")
-                                .bold()
-                        }
-                    }
-                }
-                .alert(isPresented: $showAlert) {
-                    Alert(
-                        title: Text("Done?"),
-                        primaryButton: Alert.Button.destructive(Text("Done")) {
-                            voyageSheetShow.toggle()
-                            //saveVoyage()
-                            voyageName = ""
-                            initialGold = ""
-                            initialDoubloons = ""
-                            finalGold = ""
-                            finalDoubloons = ""
-                        },
-                        secondaryButton: Alert.Button.cancel(Text("Continue")) {
-                            managerClass.start()
-                        })
-                }
+            .alert(isPresented: $showAlert) {
+                Alert(
+                    title: Text("Done?"),
+                    primaryButton: Alert.Button.destructive(Text("Done")) {
+                        voyageSheetShow.toggle()
+                        //saveVoyage()
+                        voyageName = ""
+                        initialGold = ""
+                        initialDoubloons = ""
+                        finalGold = ""
+                        finalDoubloons = ""
+                    },
+                    secondaryButton: Alert.Button.cancel(Text("Continue")) {
+                        managerClass.start()
+                    })
             }
         }
     }
